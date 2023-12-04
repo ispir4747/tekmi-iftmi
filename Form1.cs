52902e5024a0b3aead3c2a61bee6135f8bdd64a6
ispir4747 <sehmusispir11@gmail.com>
2023-12-04 19:03:29 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekmi_çiftmi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say1, kalan;
+            say1=Convert.ToInt32(textBox1.Text);
+            kalan = say1 % 2;
+            if(kalan == 1)
+            {
+                label1.Text = "sayı cift";
+
+            }
+            else
+            {
+                label1.Text = "sayı tek";
+            }
+        }
+    }
+}
